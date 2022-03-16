deb797e60466cdfe1492d9e43c6bff52442d289e
tquangdo <trangia61@gmail.com>
2022-03-17 01:47:23 +0900

    Update install.sh

--- terminal/install.sh
+++ terminal/install.sh
@@ -2,16 +2,21 @@ which python3
 # /usr/local/bin/python3
 which node
 # /usr/local/bin/node
+# if ERR: https://stackoverflow.com/questions/31691626/cant-brew-install-node
 which npm 
 # /usr/local/bin/npm
+### composer: need install PHP!!!
 which composer
 # /usr/local/bin/composer
+composer --version
+# Composer version 2.2.4 2022-01-08 12:30:42
 which aws    
 # /usr/local/bin/aws
-###
-# PHP
+### PHP
 brew install php
 brew services restart php
 # ==> Successfully started `php` (label: homebrew.mxcl.php)
 which php
 # /usr/local/bin/php
+php -v
+# PHP 8.1.3 (cli) (built: Mar  1 2022 09:21:02) (NTS)
