cccb427ffe04ca688be7e81abbb3094f6564e806
tquangdo <trangia61@gmail.com>
2022-03-17 01:12:39 +0900

    Create install.sh

---
+++ terminal/install.sh
@@ -0,0 +1,17 @@
+which python3    
+# /usr/local/bin/python3
+which node
+# /usr/local/bin/node
+which npm 
+# /usr/local/bin/npm
+which composer
+# /usr/local/bin/composer
+which aws    
+# /usr/local/bin/aws
+###
+# PHP
+brew install php
+brew services restart php
+# ==> Successfully started `php` (label: homebrew.mxcl.php)
+which php
+# /usr/local/bin/php
