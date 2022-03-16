ddcd7a30f2d1ce643562b6b43d499a1bc79d9c3c
tquangdo <trangia61@gmail.com>
2022-03-17 01:18:55 +0900

    Create path.sh

---
+++ terminal/path.sh
@@ -0,0 +1,3 @@
+echo $PATH
+# /Users/NC00011462/.yarn/bin:/Users/NC00011462/.config/yarn/global/node_modules/.bin:/usr/local/opt/php@7.3/sbin:/usr/local/opt/php@7.3/bin:/usr/local/opt/swig@3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/Library/Apple/usr/bin:/Users/NC00011462/.ebcli-virtual-env/executables:/Users/NC00011462/.yarn/bin:/Users/NC00011462/.config/yarn/global/node_modules/.bin:/usr/local/opt/php@7.3/sbin:/usr/local/opt/php@7.3/bin:/usr/local/opt/swig@3/bin
+printenv
