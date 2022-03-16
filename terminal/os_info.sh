e50836f23f76ca5b0cff4c0064b76e107e919b7e
tquangdo <trangia61@gmail.com>
2022-03-17 00:48:30 +0900

    Create os_info.sh

---
+++ terminal/os_info.sh
@@ -0,0 +1,27 @@
+sw_vers
+=>
+#
+ProductName:    macOS
+ProductVersion: 12.1
+BuildVersion:   21C52
+system_profiler SPSoftwareDataType
+#
+=>
+#
+Software:
+
+    System Software Overview:
+
+      System Version: macOS 12.1 (21C52)
+      Kernel Version: Darwin 21.2.0
+      Boot Volume: Macintosh HD
+      Boot Mode: Normal
+      Computer Name: N20LT956
+      User Name: TRAN QUANG DO (NC00011462)
+      Secure Virtual Memory: Enabled
+      System Integrity Protection: Enabled
+      Time since boot: 3 days 6:57
+#
+uname -a
+=>
+# Darwin N20LT956.local 21.2.0 Darwin Kernel Version 21.2.0: Sun Nov 28 20:28:54 PST 2021; root:xnu-8019.61.5~1/RELEASE_X86_64 x86_64
