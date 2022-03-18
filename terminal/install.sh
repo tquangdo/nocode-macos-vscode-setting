757fa7400065ea5f946d0bc74f449c55f82e5c25
tquangdo <trangia61@gmail.com>
2022-03-18 13:08:38 +0900

    Update install.sh

--- terminal/install.sh
+++ terminal/install.sh
@@ -8,9 +8,22 @@ which node
 # if ERR: https://stackoverflow.com/questions/31691626/cant-brew-install-node
 which npm 
 # /usr/local/bin/npm
+### aws-cli
+# 1/ install
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html#:~:text=Linux-,macOS,-Windows
+# 2/
 which aws    
 # /usr/local/bin/aws
+aws --version
+# aws-cli/2.2.15 Python/3.8.8 Darwin/21.2.0 exe/x86_64 prompt/off
 ### composer: need install PHP!!!
+# 1/ install
+# https://getcomposer.org/doc/00-intro.md#installation-linux-unix-macos
+# 2/ after install
+cat ~/.zshenv 
+# export PATH=~/.composer/vendor/bin:$PATH
+ls ~/.composer/vendor/bin # laravel
+# 3/
 which composer
 # /usr/local/bin/composer
 composer --version
